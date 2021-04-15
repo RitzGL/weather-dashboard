b62b69d82034c32b50a18dba9f193bf821ac4bc3
Ritz GL <maurigarzonlara@hotmail.com>
2021-04-16 02:25:06 +0800

    relevant data fetched correctly, still have to display it

--- js/script.js
+++ js/script.js
@@ -2,21 +2,47 @@ let searchButton = document.querySelector("#search-btn")
 let searchCity = "perth" // this will change later, it's perth for testing purposes
 const apiKey = "56eeec31f54f0e69895a4257692aa2f4"
 const wthrURL = "https://api.openweathermap.org/data/2.5/"
-let currentDate = moment().format("D, M, YY") // get the current date
-
-console.log("date from moment: ", currentDate) // logs as day, month, year
 
 // single object to populate the html elements
 let city = {
-    date: "",
-    coordinates: {
-        lat: 0,
-        lon: 0
-    },
+    date: moment().format("D/M/YY"),
     temp: 0,
     wind: 0,
     humidity: 0,
-    uv: 0
+    uv: 0,
+    // iterate over this array and the daily property of "weather" to populate with 5 elements(i=4)
+    forecast: [
+        {
+            temp: 0,
+            wind: 0,
+            humidity: 0,
+            uv: 0
+        },
+        {
+            temp: 0,
+            wind: 0,
+            humidity: 0,
+            uv: 0
+        },
+        {
+            temp: 0,
+            wind: 0,
+            humidity: 0,
+            uv: 0
+        },
+        {
+            temp: 0,
+            wind: 0,
+            humidity: 0,
+            uv: 0
+        },
+        {
+            temp: 0,
+            wind: 0,
+            humidity: 0,
+            uv: 0
+        }
+    ]
 }
 
 // object to select dashboard elements on the html
@@ -47,62 +73,71 @@ function getCoordinates(){
     .then((response) => response.json())
 
     // access the information from the js object
+    // perhaps calling getWeather in here will help with some asynch issues
+    // given that this block will execute once the promise is resolved
+    // and seeing that i keep trying to access things not yet defined (after this is resolved)
+    // i may want to access them after i have received this information in the first place
+    // AKA not trying to access data not given by this function
     .then((weather) => {
-        // console.log(weatherData)
-        localStorage.setItem("Lat", weather.coord.lat)
-        localStorage.setItem("Lon", weather.coord.lon)
+        // store latitude and longitude to later inject in second fetch request
+        // co.lat = weather.coord.lat
+        // co.lon = weather.coord.lon
+        getWeather(weather.coord.lat, weather.coord.lon)
     })
-
-    // retrieve coordinates from localStorage, store into city object
-    city.coordinates.lat = localStorage.getItem("Lat")
-    city.coordinates.lon = localStorage.getItem("Lon")
-
-    // remove temporary coordinates from storage to free up space
-    localStorage.removeItem("Lat")
-    localStorage.removeItem("Lon")
 }
 
 // request weather information to be displayed on the elements
-function getWeather(){
+// then store JSON string into local storage (must parse object for use)
+function getWeather(lat, lon){
 
-    fetch(`${wthrURL}onecall?lat=${city.coordinates.lat}&lon=${city.coordinates.lon}&units=metric&appid=${apiKey}`)
+    fetch(`${wthrURL}onecall?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`)
 
     // get the information as a json object
     .then((response) => response.json())
 
     // access the information from the js object
     .then((weather) => {
-        console.log("second fetch data:", weather)
+        // read data into my object from returned values
+        console.log(weather)
+        city.temp = weather.current.temp
+        city.wind = weather.current.wind_speed
+        city.humidity = weather.current.humidity
+        city.uv = weather.current.uvi
+        for(let i = 0; i < 5; i++){
+            city.forecast[i].temp = weather.daily[i].temp.day
+            city.forecast[i].wind = weather.daily[i].wind_speed
+            city.forecast[i].humidity = weather.daily[i].humidity
+            city.forecast[i].uv = weather.daily[i].uvi
+        }
+        localStorage.setItem("City", JSON.stringify(city))
+    })
+    .then(() => {
         
+        // my assumptions on top were correct
+        // i need to chain this so i can properly access the valid data received after the promise is resolved
+        // write general functions to insert relevant DOM elements
+        // and display relevant information to the user.
+        // these general insertion functions will be invoked here
+        let storedString = localStorage.getItem("City")
+
+        let storedCity = JSON.parse(storedString)
+
+        console.log("stored object",storedCity)
+        console.log("stored string",storedString)
     })
 
-    // this is how i will read the information back into this environment
-    // city.coordinates.lat = localStorage.getItem("Lat")
-    // city.coordinates.lon = localStorage.getItem("Lon")
+    //this will display the relevant information to the DOM
+    // dashboard.date.textContent = storedCity.date
+    // dashboard.temp.textContent = storedCity.temp
+    // dashboard.wind.textContent = storedCity.wind
+    // dashboard.humidity.textContent = storedCity.humidity
+    // dashboard.uv.textContent = storedCity.uv
 }
-
-
-// this clears whatever memory is there
-// only call after values read from weatherData are properly read
-// localStorage.clear()
-
-// console.log(coordinates)
 
 getCoordinates();
 
-getWeather();
 
-console.log(city)
-
-// get one call request with lat and lon
-// create relevant objects based on needed info
-    // on dashboard
-        // temp
-        // icon
-        // wind
-        // humidity
-        // uv index
-    // on 5-day forecast
+// on 5-day forecast
         // Date
         // icon
         // temp
